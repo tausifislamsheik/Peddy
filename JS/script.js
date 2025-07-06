a9a026cf0c0a23b14158df3fefbb1f37a5923fd2
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-07-06 23:58:25 +0600

    Category section added

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ const navbarContainer = () =>{
             <img class='w-6' src='images/logo.webp' />
             <p class='text-2xl font-bold'>Peddy</p>
         </div>
-        <div class="navbar-center hidden lg:flex list-none gap-5 font-semibold cursor-pointer text-gray-500">
+        <div class="navbar-center hidden lg:flex list-none gap-5 font-semibold cursor-pointer">
             <li class='hover:text-teal-800'>Home</li>
             <li class='hover:text-teal-800'>Shop</li>
             <li class='hover:text-teal-800'>Contact</li>
@@ -28,8 +28,8 @@ const posterContainer = () =>{
     const posterSection = document.querySelector('#poster-section');
     const poster = document.createElement('div');
     poster.innerHTML =`
-         <p class='text-2xl font-bold text-gray-500 mb-5'>Bringing Families Together ❤️‍🔥 </p>
-         <p class='text-5xl font-bold mb-5'>Your Path to Adoption <br> Starts Here</p>
+         <p class='text-sm lg:text-2xl font-bold text-gray-500 mb-5'>Bringing Families Together ❤️‍🔥 </p>
+         <p class='text-3xl lg:text-6xl font-bold mb-5'>Your Path to Adoption <br> Starts Here</p>
          <p class='text-gray-500 mb-5'>It is a long established fact that a reader will be distracted by the readable content of a page <br> when looking at its layout. The point of using Lorem Ipsum is that it has a.</p>
          <button class="btn btn-outline hover:bg-teal-900 hover:text-white border-teal-700 text-teal-700">View More</button>
          <img class='w-[600px] mx-auto' src='images/pet.webp' />
@@ -37,5 +37,31 @@ const posterContainer = () =>{
     posterSection.appendChild(poster);
 };
 
+// Category section
+
+const loadCategory = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
+    const data = await res.json();
+    categoryContainer(data.categories)
+}
+
+const categoryContainer = (categories) =>{
+    const categorySection = document.getElementById('category-section');
+    categories.forEach(category => {
+        const categoryBtn = document.createElement('div');
+        categoryBtn.innerHTML = `
+            
+            <button class='btn px-12 py-8'>
+                ${category.category}
+            </button>
+        `
+        categorySection.append(categoryBtn)
+    });
+}
+
+
+
+
+loadCategory();
 posterContainer();
 navbarContainer();
